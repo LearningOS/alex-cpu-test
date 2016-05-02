1a9a110ef468839dc6777d77709062aa9c897816
Alex Wang <ice_b0und@hotmail.com>
2016-05-02 21:52:16 +0800

    finish alex machine hello world test

--- src/start.c
+++ src/start.c
@@ -1,8 +1,40 @@
 asm(".global _start");
 asm("_start:");
-asm("li  $t0, 0x0000");
-asm("lih $t0, 0x07c0  # t0 = 128M - 4M");
-asm("addi $sp, $t0, 0");
-asm("li  $t0, %lo(main)");
-asm("lih $t0, %hi(main)");
-asm("jr  $t0");
+asm("li $t0, 0");
+asm("li $t1, 0");
+asm("li $t2, 0");
+asm("li $t3, 0");
+asm("li $t4, 0");
+asm("li $s0, 0");
+asm("li $s1, 0");
+asm("li $s2, 0");
+asm("li $s3, 0");
+asm("li $s4, 0");
+asm("li $fp, 0");
+
+asm("li $t1, 0x0  # t1 = 128M - 4M");
+asm("lih $t1, 0x07c0  # t1 = 128M - 4M");
+asm("addi $sp, $t1, 0");
+
+// call main
+asm("lih $t1, %hi(main)");
+asm("addiu  $t1, $t1, %lo(main)");
+asm("jr  $t1");
+
+// print hello
+asm("li $t0, 0");
+asm("li $t1, 72");
+asm("bout $t0, $t1");
+asm("li $t1, 101");
+asm("bout $t0, $t1");
+asm("li $t1, 108");
+asm("bout $t0, $t1");
+asm("li $t1, 108");
+asm("bout $t0, $t1");
+asm("li $t1, 111");
+asm("bout $t0, $t1");
+asm("li $t1, 10");
+asm("bout $t0, $t1");
+
+
+asm("halt");
--- src/test_link.c
+++ src/test_link.c
@@ -1,6 +1,18 @@
 extern int test_switch(int a, int *ary);
 int intv = 222;
 int intary[100] = { 0 };
-void main() {
-  intv = test_switch(123, intary);
+
+void putc(int c) {
+  int port = 1;
+  asm volatile (
+    "bout %0, %1\n\t"
+    :
+    : "r" (port), "r" (c)
+  );
+}
+
+int main() {
+  putc('H');putc('e');putc('l');putc('l');putc('o');putc('!');
+  //intv = test_switch(123, intary);
+  return 0;
 }
